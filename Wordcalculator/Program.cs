b2d0fb9adcda7c08f11bd132a2e5508e96a64fb9
Myseie <123580372+Myseie@users.noreply.github.com>
2024-09-06 09:37:52 +0200

    Add files via upload

---
+++ Wordcalculator/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Skriv in din mening här :");
+
+string input = Console.ReadLine();
+
+string[] words = input.Split(' ');
+
+
+int counter = 0;  
+
+foreach (string word in words)
+{
+    counter++;
+}
+
+Console.WriteLine("Antalet ord :" + counter);
